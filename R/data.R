2a9d758b552adcf2a19d1c5a86ae89a096ca0104
Iyed Ghedamsi <contact@iyedg.me>
2022-07-10 23:11:50 +0100

    Initial commit

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Consumer Price Index.
+#'
+#' A dataset containing the consumer price index as measured by the
+#' Tunisian Statistics' National Institute (INS) since 1962.
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{base_year}{Base year used for measurements. This is important as a change in base year reflects a change in the consumer basket used.}
+#'   \item{cpi}{The value of the Consumer Price Index}
+#'   \item{measurement_date}{The date of measurement (Month)}
+#' }
+#' @source \url{"http://www.ins.tn/publication/indice-des-prix-la-consommation"}
+"consumer_price_index"
---
+++ data-raw/consumer_price_index.R
@@ -0,0 +1,106 @@
+## code to prepare `consumer_price_index` dataset goes here
+library(rvest)
+library(dplyr)
+library(tidyr)
+library(readxl)
+library(lubridate)
+library(conflicted)
+library(stringr)
+
+conflict_prefer("filter", "dplyr")
+
+months <- c(
+  "janvier",
+  "fevrier",
+  "mars",
+  "avril",
+  "mai",
+  "juin",
+  "juillet",
+  "aout",
+  "septembre",
+  "octobre",
+  "novembre ",
+  "decembre"
+)
+
+reference_date <- today() %m-% months(1)
+
+year <- year(reference_date)
+month <- months[month(reference_date)]
+
+url_template <- "http://www.ins.tn/publication/indice-des-prix-la-consommation-{month}-{year}"
+
+html_content <- rvest::read_html(glue(url_template))
+
+data_url <- html_content %>%
+  html_element(".file.file--mime-application-vnd-openxmlformats-officedocument-spreadsheetml-sheet.file--x-office-spreadsheet.icon-before") %>%
+  html_element("a") %>%
+  html_attr("href")
+httr::GET(data_url, httr::write_disk(tf <- tempfile(fileext = ".xlsx")))
+
+
+raw_consumer_price_index <- read_excel(tf, sheet = 2, skip = 11)
+
+
+DATE_ORIGIN <- "1899-12-30"
+
+month_to_n_fr <- c(
+  "Janvier" = "1",
+  "Février" = "2",
+  "Mars" = "3",
+  "Avril" = "4",
+  "Mai" = "5",
+  "Juin" = "6",
+  "Juillet" = "7",
+  "Août" = "8",
+  "Septembre" = "9",
+  "Octobre" = "10",
+  "Novembre" = "11",
+  "Décembre" = "12"
+)
+
+
+consumer_price_index <- raw_consumer_price_index %>%
+  janitor::clean_names() %>%
+  filter(annee != "Mois", annee != "Moyenne annuelle") %>%
+  select(-mensuelle) %>% # Unused column
+  rename(measurement_date = annee) %>%
+  mutate(
+    measurement_year = case_when(
+      str_detect(measurement_date, regex("^\\d{4}$")) ~ measurement_date,
+      TRUE ~ NA_character_
+    ), # The year is available only in some rows, moved to another column to be later filled downward
+    measurement_date = case_when(
+      str_detect(
+        measurement_date,
+        regex("^\\d{5}$")
+      ) ~ as.character(
+        month(as.Date(as.numeric(measurement_date), DATE_ORIGIN))
+      ),
+      TRUE ~ as.character(measurement_date)
+    ) # Values starting from 2014 are read as Excel dates, this mutate harmonizes them
+  ) %>%
+  tidyr::fill(measurement_year, .direction = "down") %>%
+  filter(str_detect(measurement_date, regex("^\\d{4}$"), negate = T)) %>%
+  mutate(
+    measurement_date = str_replace_all(measurement_date, month_to_n_fr),
+    measurement_date = str_c(measurement_date, measurement_year, sep = "-"),
+    measurement_date = lubridate::my(measurement_date)
+  ) %>%
+  select(-measurement_year) %>%
+  pivot_longer(
+    cols = -c(measurement_date),
+    names_to = "base_year",
+    values_to = "cpi",
+    names_transform = list(base_year = as.character),
+    values_transform = list(cpi = as.character)
+  ) %>%
+  mutate(
+    cpi = as.numeric(cpi),
+    base_year = as.numeric(str_remove(base_year, "x"))
+  ) %>%
+  arrange(base_year, measurement_date)
+
+
+usethis::use_data(consumer_price_index, overwrite = TRUE)
