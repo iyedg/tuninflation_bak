3425e35866f7b1ac151679412bccbcfee4da3816
Iyed Ghedamsi <iyed.ghedamsi@gmail.com>
2022-08-08 14:16:22 +0100

    Inflation By Item Dataset (since 2015)

---
+++ data-raw/item_inflation.R
@@ -0,0 +1,46 @@
+pacman::p_load(
+  here,
+  dplyr,
+  magrittr,
+  tidyr,
+  ggplot2,
+  readxl,
+  readr,
+  janitor,
+  conflicted,
+  stringr,
+  fuzzyjoin,
+  gt,
+  gtExtras
+)
+
+load(here("data/ins_coicop.rda"))
+
+
+item_inflation <- read_excel(
+  path = here::here("data-raw/ipc_july_2022.xlsx"),
+  sheet = "COICOP",
+  skip = 6
+) %>%
+  clean_names() %>%
+  select(-contains("moyenne")) %>%
+  rename(
+    ins_category = x1,
+    weight = pond
+  ) %>%
+  pivot_longer(
+    cols = -c(ins_category, weight),
+    names_to = "measurement_period",
+    values_to = "cpi"
+  ) %>%
+  mutate(
+    measurement_period = as.numeric(str_remove(measurement_period, "x")),
+    measurement_period = excel_numeric_to_date(measurement_period),
+    cpi = as.numeric(cpi)
+  ) %>%
+  mutate(
+    monthly_item_inflation = (cpi - lag(cpi, 1)) / lag(cpi, 1),
+    yearly_item_inflation = (cpi - lag(cpi, 12)) / lag(cpi, 12)
+  )
+
+usethis::use_data(item_inflation, overwrite = TRUE)
